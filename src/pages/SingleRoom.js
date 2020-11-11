30aea60a319db9b8edda07ff8aa1ed35e7819b03
Gwangwoo Kim <gwkim8107@gmail.com>
2020-11-12 11:36:49 +1300

    add  SingleRoom.js

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../context';
 
 class SingleRoom extends Component {
+    constructor(props){
+        super(props);
+        console.log("props : ", props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        };
+    }
+    static contextType = RoomContext;
+    componentDidMount(){
+    
+    }
+
     render() {
-        return (
-            <div>
-                hello from single room page
-            </div>
-        )
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        // console.log(room);
+        if(!room){
+            return (
+                <div className="error">
+                    <h3>No such room could be found...</h3>
+                    <Link to="/rooms" className="btn-primary">
+                        Back to rooms
+                    </Link>
+                </div>
+            );
+        }
+        const {name, description, capcity, size, price, extras, breakfasrt, pets, images} = room;
+
+        return ( 
+            <Hero hero='roomsHero'>
+                <Banner title={`${name} room`}>
+                    <Link to="/rooms" className="btn-primary">
+                        back to rooms
+                    </Link>
+                </Banner> 
+            </Hero>
+            )
     }
 }
 
 export default SingleRoom;
+
+//2:47:00-
