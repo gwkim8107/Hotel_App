e4943f1e75b7ef05a17f47f6bd395687dcfc7422
Gwangwoo Kim <gwkim8107@gmail.com>
2020-11-12 11:34:46 +1300

    updated context.js

--- src/context.js
+++ src/context.js
@@ -21,7 +21,10 @@ class RoomProvider extends Component {
         let rooms = this.formatData(items)
         let featuredRooms = rooms.filter( room => room.featured === true);
         this.setState({
-            rooms, featuredRooms, sortedRooms: rooms, loading: false
+            rooms, 
+            featuredRooms, 
+            sortedRooms: rooms, 
+            loading: false
         })
         // console.log(rooms);
     } 
@@ -36,9 +39,19 @@ class RoomProvider extends Component {
         return tempItems;
     }
 
+    getRoom = slug => {
+        let tempRooms = [...this.state.rooms];
+        const room = tempRooms.find( (room) => room.slug === slug);
+        return room;
+    };
+
     render() {
         return (
-            <RoomContext.Provider value={{...this.state}}>
+            <RoomContext.Provider 
+                value={{
+                    ...this.state,
+                    getRoom: this.getRoom 
+                }}>
                 {this.props.children}
             </RoomContext.Provider>
         )
